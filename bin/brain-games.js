05e5e130f87bc8673d8aae24b7a6830aa58bd010
Elijah <ilyasolonovich20@gmail.com>
2021-10-25 21:07:52 +0300

    update

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!');
+import { greet } from '../src/cli.js';
+//import readlineSync from 'readline-sync';
+
+//let name = readlineSync.question('May I have your name? ');
+
+const name = greet();
+console.log('Hello, ' + name + '!');
