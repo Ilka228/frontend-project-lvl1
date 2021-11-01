4517f4fdee0e7ad5b8a6f343f08a874d77799baf
Elijah <ilyasolonovich20@gmail.com>
2021-11-01 13:07:30 +0300

    update

---
+++ bin/brain-gcd.js
@@ -0,0 +1,36 @@
+import answer from '../src/cli.js';
+import { name1 } from './brain-games.js';
+
+function getRandomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const section1 = 1;
+const section2 = 1000;
+const gcd = (a, b) => {
+  if (!b) {
+    return a;
+  }
+
+  return gcd(b, a % b);
+};
+
+const condition = 'What is the result of the expression?';
+console.log(condition);
+for (let i = 1; i < 4; i += 1) {
+  const number1 = getRandomInRange(section1, section2);
+  const number2 = getRandomInRange(section1, section2);
+  console.log(`Question: ${number1} ${number2}`);
+  // console.log(eval(number1 + item + number2));
+  const certanswer = Number(answer());
+  if (Number(gcd(number1, number2)) === certanswer) {
+    console.log('Correct!');
+    if (i === 3) {
+      console.log(`Congratulations, ${name1}!`);
+    }
+  } else {
+    console.log(`'${certanswer}' is wrong answer ;(. Correct answer was '${gcd(number1, number2)}'.`);
+    console.log(`Let's try again, ${name1}!`);
+    break;
+  }
+}
