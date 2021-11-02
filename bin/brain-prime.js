cf2625e08775c831df2aea928c4a476902d610c5
Elijah <ilyasolonovich20@gmail.com>
2021-11-02 23:00:33 +0300

    update

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import {launchprime} from '../src/index.js';
+import { launchprime } from '../src/index.js';
+
 const section1 = 2;
 const section2 = 1000;
 const condition = 'Answer "yes" if given number is prime. Otherwise answer "no".';
